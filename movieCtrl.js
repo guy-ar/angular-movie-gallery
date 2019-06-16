d9514cee68769bbd5a93cbb909366438e4bae722
guyar <guy.arbilly@gmail.com>
2019-06-16 16:03:03 +0300

    upload version for additioanl search for movies - not working yet

--- movieCtrl.js
+++ movieCtrl.js
@@ -48,4 +48,59 @@ app.controller("movieCtrl", function($scope, $http) {
           console.error(err);
         });
 
+    // Initializing variables
+    $scope.movieSearchText = "";
+    $scope.searchResults = [];
+    $scope.favoriteMovies = [];
+
+    $scope.updateSearchResults = function() {
+        if ($scope.movieSearchText) {
+        
+        let apiKey = "?api_key=bf17da39659009eb552f15e8ebda08ad";
+        let prefixUrl = "https://api.themoviedb.org/3/";
+        //let searchOptionActor = "search/person"; --> used for actors
+        let searchOptionActor = "search/movie";
+        let searchQuery = "&query=" + $scope.movieSearchText;
+        let fullSearchUrl = prefixUrl + searchOptionActor + apiKey + searchQuery;
+        /* example for search movies
+        https://api.themoviedb.org/3/search/movie?api_key=bf17da39659009eb552f15e8ebda08ad&language=en-US&query=avengers&page=1&include_adult=false
+        */
+        
+        // example for actor: 
+        /*https://api.themoviedb.org/3/search/person?api_key=53d2ee2137cf3228aefae083c8158855&query=" 
+            + $scope.movieSearchText;*/
+
+        $http.get(fullSearchUrl).then(function(res) {
+                $scope.searchResults = res.data.results;
+                console.log($scope.searchResults);
+            }, function(err) {
+                console.error(err);
+            })
+        
+        } else {
+            $scope.searchResults = [];  // This code creates a new empty array
+        }
+    }
+
+    // Adding movie (getting details from TMDB)
+    $scope.addMovie = function(searchResult) {
+
+        var movieDetailsUrl = "https://api.themoviedb.org/3/movie/" + 
+        searchResult.id + apiKey;
+        
+        $http.get(movieDetailsUrl).then(function(res) {
+            var movie = new Movie(
+                //  need to get the following data: name, imdbId, releasedDate, length, poster, starsArr, director
+                // failed to get the start and director - need to see how top get the image
+            
+            res.data.title, res.data.imdb_id, res.data.release_date, res.data.runtime, "https://image.tmdb.org/t/p/w500" + res.data.poster_path, [], "");
+            $scope.movies.push(actor);
+        }, function(err) {
+            console.error(err);
+        })
+        
+        // Cleaning the search result 
+        $scope.movieSearchText = "";
+        $scope.searchResults = [];
+    }
 });
